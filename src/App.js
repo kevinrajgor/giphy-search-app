e758d2cd7e016cdb5f69d0966eb048df506a5cc0
kevinrajgor <kevinrajg95@gmail.com>
2023-05-13 13:58:30 +0530

    Integrated Items component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import "./App.css";
+import image from "./Assets/search-icon.svg";
+import Items from "./Components/Items";
 
 function App() {
-  return <div>Not Rendered!</div>;
+  const [data, setData] = useState([]);
+  const searchQueryRef = useRef("");
+  const [timer, setTimer] = useState();
+
+  function handleChange(e) {
+    let query = e.currentTarget.value;
+    searchQueryRef.current = query;
+    // console.log(query);
+    clearTimeout(timer);
+    setTimer(
+      setTimeout(() => {
+        fetch(
+          `https://api.giphy.com/v1/gifs/search?q=${searchQueryRef.current}&api_key=GlVGYHkr3WSBnllca54iNt0yFbjz7L65&limit=3`
+        )
+          .then((res) => res.json())
+          .then((res) => setData(res.data));
+        console.log("api called");
+      }, 300)
+    );
+  }
+
+  return (
+    <div className="main-content">
+      <section>
+        <div className="searchbar">
+          <img
+            src={image}
+            height="25px"
+            alt="search icon"
+            id="searchbar-icon"
+          />
+          <input
+            placeholder="Article name or keywords"
+            id="searchbar-bar"
+            type="text"
+            onChange={handleChange}
+            autoComplete="off"
+          />
+          <button id="searchbar-button">Search</button>
+        </div>
+        <div className="container">
+          {data.map((item) => {
+            return <Items item={item} />;
+          })}
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default App;
