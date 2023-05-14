a11dbfee6c4dcfbf65b4fef184b0f18a949d3061
kevinrajgor <kevinrajg95@gmail.com>
2023-05-14 22:28:44 +0530

    Added set favourite aesthetics

--- src/Components/Items.jsx
+++ src/Components/Items.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Items({ item }) {
+  const [isClicked, setIsClicked] = useState(false);
   return (
     <div className="items">
       <img
@@ -9,8 +10,31 @@ function Items({ item }) {
         height="300px"
         width="300px"
       />
-      <p>{item.user ? item.user.display_name : ""}</p>
-      <span>@{item.user ? item.user.username : ""}</span>
+      <div className="user-content">
+        <div className="user-details">
+          <p>{item.user ? item.user.display_name : "N/A"}</p>
+          <span>@{item.user ? item.user.username : "N/A"}</span>
+        </div>
+        <div className="star">
+          {!isClicked ? (
+            <img
+              width="48"
+              height="48"
+              src="https://img.icons8.com/color/48/star--v1.png"
+              alt="star--v1"
+              onClick={() => setIsClicked(!isClicked)}
+            />
+          ) : (
+            <img
+              width="48"
+              height="48"
+              src="https://img.icons8.com/color/48/filled-star--v1.png"
+              alt="filled-star--v1"
+              onClick={() => setIsClicked(!isClicked)}
+            />
+          )}
+        </div>
+      </div>
     </div>
   );
 }
